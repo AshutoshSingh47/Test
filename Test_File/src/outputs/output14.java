7b2821dedd024ccb8a7c4e173f03756430224ebb
muske <ashutoshsingh.iimt@gmail.com>
2022-09-08 23:11:44 +0530

    new program

---
+++ Test_File/src/outputs/output14.java
@@ -0,0 +1,28 @@
+package outputs;
+
+//abstract class Todd{
+//	
+//	Todd(){
+//		System.out.print("bongo bongo");
+//	}
+//	public static void show() {
+//		
+//	}
+//}
+
+public class output14{
+	
+//	output14()
+//	{
+//		super();
+//	}
+//	public static void show() {
+//		System.out.print("Toc");
+//	}
+	
+	public static void main(String[] args) {
+		
+	  
+	}
+
+}
