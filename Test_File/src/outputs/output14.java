64a093220c2c0afafe3f7fb2ab7330f1fd9c105e
muske <ashutoshsingh.iimt@gmail.com>
2022-09-21 19:20:42 +0530

    committed

--- Test_File/src/outputs/output14.java
+++ Test_File/src/outputs/output14.java
@@ -1,28 +1,34 @@
 package outputs;
 
-//abstract class Todd{
-//	
-//	Todd(){
-//		System.out.print("bongo bongo");
-//	}
-//	public static void show() {
-//		
-//	}
-//}
+abstract class A1{
+	int show() {
+		return 0;
+	}
+	static int show(int n) {
+		return 0;
+	}
+	
+}
 
-public class output14{
-	
-//	output14()
-//	{
-//		super();
-//	}
-//	public static void show() {
-//		System.out.print("Toc");
-//	}
-	
+abstract class A2{
+	int show() {
+		return 0;
+	}
+}
+
+
+public class output14 extends A1 {
+
 	public static void main(String[] args) {
 		
-	  
+		
+	}
+
+	@Override
+	int show() {
+		
+		return 1;
 	}
 
 }
+
