0657cf574b5bc47b30046bd1888cc0fc3e236d29
AshutoshSingh47 <ashutoshsingh.iimt@gmail.com>
2022-11-04 17:29:13 +0530

    added pattern

---
+++ Test_File/src/outputs/output20.java
@@ -0,0 +1,126 @@
+package outputs;
+import java.util.Scanner; 
+public class output20 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter limit : ");
+		int n=sc.nextInt();
+		int i,j,k,sp=n;
+		
+		//first half
+		for(i=0;i<(n/2)+1;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			if(i==n/2)
+			{
+				for(k=0;k<n;k++)
+				{
+					System.out.print("@");
+				}
+				System.out.print("*");
+				
+				
+				
+			}
+			System.out.println();
+		}
+		
+		for(i=(n/2)+1;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			
+			for(j=0;j<sp;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			System.out.print("*");
+			
+			for(j=0;j<n;j++)
+			{
+				System.out.print(" ");
+			}
+			for(k=0;k<n;k++)
+			{
+				System.out.print("@");
+			}
+			
+			
+			System.out.println();
+			
+		}
+		
+		for(i=0;i<n+sp;i++)
+		{
+			System.out.print(" ");
+		}
+		for(i=0;i<n+1;i++)
+		{
+			System.out.print("*");
+		}
+		
+		for(i=0;i<n;i++)
+		{
+			System.out.print("@");
+		}
+		
+		System.out.println();
+		
+		//second half
+		
+		for(i=0;i<(n/2);i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			
+			
+			for(j=0;j<sp;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			System.out.print("*");
+			
+			for(j=0;j<n;j++)
+			{
+				System.out.print(" ");
+			}
+			for(k=0;k<n;k++)
+			{
+				System.out.print("@");
+			}
+			
+			System.out.println();
+		}
+		
+		for(i=(n/2);i<n;i++)
+		{
+			
+			for(j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			if(i==(n/2))
+			{
+				for(k=0;k<n;k++)
+				{
+					System.out.print("@");
+				}
+				System.out.print("*");
+				
+				
+			}
+			System.out.println();
+		}		
+	}
+
+}
